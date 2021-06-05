79993a2991bb788cbf0882fcae36c3d8afadf1fb
Nelly <nelly.quino17@tectijuana.edu.mx>
2021-06-04 22:54:00 -0700

    listas_tratamientod, docotores, pacientes

--- Clinica/urls.py
+++ Clinica/urls.py
@@ -12,19 +12,24 @@ urlpatterns = [
     path('editar_usuario/<int:pk>', views.EditarUsuario.as_view(), name="editar_usuario"),
     path('detalles_usuario/<int:pk>', views.DetallesUsuario.as_view(), name="detalles_usuario"),
     path('borrar_usuario/<int:pk>', views.BorrarUsuario.as_view(), name="borrar_usuario"),
+    path('lista_tratamientos/', views.ListaTratamiento.as_view(), name="lista_tratamientos"),
+    path('editar_tratamiento/', views.EditarTratamiento.as_view(), name="editar_tratamiento"),
     #Recepcionista
+    path('lista_pacientes/', views.ListaPacientes.as_view(), name="lista_pacientes"),
+    path('editar_paciente/<int:pk>/', views.EditarPaciente.as_view(), name="editar_paciente"),
     #Doctor
+    path('lista_doctores/', views.ListaDoctores.as_view(), name="lista_doctores"),
+    path('detalles_doctor/<int:pk>/', views.DetallesDoctor.as_view(), name="detalles_doctor"),
 
     #path('agregar_horario/',views.AgregarHorario.as_view(), name="agregar_horario"),
     #path('lista_usuarios/', views.ListaUsuarios.as_view(), name="lista_usuarios"),
-    #path('lista_tratamientos/', views.ListaTratamiento.as_view(), name="lista_tratamientos"),
+
     #path('crear_tratamiento/', views.CrearTratamiento.as_view(), name="crear_tratamiento"),
-    #path('editar_tratamiento/<int:pk>/', views.EditarTratamiento.as_view(), name="editar_tratamiento"),
-    #path('lista_doctores/', views.ListaDoctores.as_view(), name="lista_doctores"),
-    #path('detalles_doctor/<int:pk>/', views.DetallesDoctor.as_view(), name="detalles_doctor"),
-    #path('lista_pacientes/', views.ListaPacientes.as_view(), name="lista_pacientes"),
+
+
+
     #path('detalles_paciente/<int:pk>/', views.DetallesPaciente.as_view(), name="detalles_paciente"),
-    #path('editar_paciente/<int:pk>/', views.EditarPaciente.as_view(), name="editar_paciente"),
+
     #path('doctor/<int:pk>/', views.PerfilDoctor.as_view(), name="doctor"),
     #path('doctor_tratamientos/', view.TramientosDoctor.as_view(), name="doctor_tratamientos"),
     #path('horario_doctor/', views.HorarioDoctor.as_view(), name="horario_doctor"),
