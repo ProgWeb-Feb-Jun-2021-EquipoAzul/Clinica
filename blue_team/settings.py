507b0828e9ea33ae5df352f9d82b25de48f4eea5
Hector Jaramillo <43076992+HectorJaramillo@users.noreply.github.com>
2021-06-14 17:22:06 -0700

    Update settings.py

--- blue_team/settings.py
+++ blue_team/settings.py
@@ -152,5 +152,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-   os.path.join(BASE_DIR / "statics")
+   os.path.join(BASE_DIR / "static-storage")
 ]
